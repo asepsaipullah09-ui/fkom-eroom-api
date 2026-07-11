4001d0d69b6f0502d388c9b638c5d245a9e47c0a
Asep Saepullah <asep.saipullah09@gmail.com>
2026-07-11 15:26:40 +0700

    Add migration files for rooms, mata_kuliah, bookings, room_facilities, approvals, notifications, and update users table structure

--- database/migrations/2026_07_11_082434_create_notifications_table.php
+++ database/migrations/2026_07_11_082434_create_notifications_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('judul');
+            $table->text('pesan');
+            $table->foreignId('booking_id')->nullable()->constrained()->onDelete('cascade');
+            $table->boolean('is_read')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('notifications');
